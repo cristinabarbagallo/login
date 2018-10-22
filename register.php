9547e1af57a841695112d4aa7c14a0a40396e3c0
Cristina <cris.barbagallo@gmail.com>
2018-10-22 09:55:47 +0200

    add register file with PDO

--- register.php
+++ register.php
@@ -1,4 +1,14 @@
 <?php
+/* Spara user i databas:
+1. Hantera post variabler: $_POST
+2. Koppling till databas: PDO
+3. Spara user i databas */
 
+
+$pdo = new PDO(
+    "mysql:host=localhost;dbname=fed18;charset=utf8",
+    "root",
+    "root"
+);
 
 ?>
